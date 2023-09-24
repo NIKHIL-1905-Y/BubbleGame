a25c769ad2616bed8ba0835f3f4f603a516229ee
unknown <yadavnishu1905@gmail.com>
2023-09-24 17:24:28 +0530

    BG1.0.0.1

---
+++ script.js
@@ -0,0 +1,63 @@
+var timer = 60;
+var score = 0;
+var hitrn = 0;
+
+//Score Increase
+function increaseScore(){
+    score = score + 10;
+    document.querySelector("#scoreval").textContent = score;
+
+}
+//New Hit
+function getNewHit(){
+   hitrn =  Math.floor(Math.random()*10);
+    document.querySelector("#hitval").textContent = hitrn;
+
+
+}
+//Timer Function
+function runTimer(){
+   var timerint= setInterval(function(){
+        if(timer>0){
+            timer--;
+            document.querySelector("#timervalue").textContent = timer;
+
+        }
+        else{
+            clearInterval(timerint);
+            document.querySelector("#pbtm").innerHTML = `<h1><i> Game Over </i> !</h1> </br> <h2>Score: ${score}</h2> `;
+        }
+      
+        
+    },1000);
+}
+//Bubble cration fuction
+function makeBubble(){
+    var clutter = "";
+
+for(var i =1;i<=75;i++)
+{
+    var rn = Math.floor(Math.random()*10);
+    clutter += `<div class="bubble">${rn}</div>`;
+
+}
+document.querySelector('#pbtm').innerHTML = clutter;
+}
+document.querySelector("#pbtm").addEventListener("click",
+function(details){
+    var clickednum = Number(details.target.textContent);
+    if(clickednum === hitrn)
+    {
+        increaseScore();
+        makeBubble();
+        getNewHit();
+    }
+})
+
+runTimer();
+makeBubble();
+getNewHit();
+
+
+
+
